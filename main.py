92f2440b8c14c155a09ba96156ed0f5e01f4cf44
Varsha Venkatasubramanian <varsha@Varshas-MacBook-Pro.local>
2023-06-29 12:06:53 +0530

    mongo project update

--- main.py
+++ main.py
@@ -17,24 +17,70 @@ client = MongoClient(
 )
 
 db = client['database']
-collection = db['eventsTest']
-
+eventsCollection = db['eventsTest']
+usersCollection = db['usersTest']
+loggedIn = False
 
 def transform_date(event):
   date = datetime.strptime(event['date'], "%Y-%m-%d")
   event['date'] = date.strftime("%B %d, %Y")
   return event
 
-
 @app.route('/')
 @app.route('/index')
 def index():
+  global loggedIn
+  if not loggedIn:
+    return redirect('/choice')
   # homepage with events board
-  events = list(collection.find().sort('date'))
+  events = list(eventsCollection.find().sort('date'))
   print(events)
   events = map(transform_date, events)
   return render_template("index.html", events=events)
 
+@app.route('/choice', methods=['GET'])
+def choice():
+  if request.method == 'GET':
+    return render_template("choice.html")
+
+@app.route('/users', methods=['GET'])
+def users():
+  global loggedIn
+  if request.method == 'GET':
+    return render_template("login.html")
+
+@app.route('/users/login', methods=['POST'])
+def login():
+  global loggedIn
+  if request.method == 'POST':
+    found = usersCollection.find_one({
+      'username': request.form['username'],
+      'password': request.form['password']
+    })
+    print(found)
+    if found is None:
+      return render_template("wrong.html")
+    else:
+      loggedIn = True
+      return redirect("/")
+
+@app.route('/users/logout', methods=['GET'])
+def logout():
+  global loggedIn
+  if request.method == 'GET':
+    loggedIn = False
+    return redirect("/")
+
+@app.route('/users/new', methods=['GET', 'POST'])
+def new_user():
+  if request.method == 'GET':
+    return render_template('register.html')
+  if request.method == 'POST':
+    usersCollection.insert_one({
+      'username': request.form['username'],
+      'password': request.form['password']
+    })
+    return redirect("/")
 
 @app.route('/events/new', methods=['POST', 'GET'])
 def new_events():
@@ -46,7 +92,7 @@ def new_events():
       'date': request.form['event_date']
     }
     print(event)
-    collection.insert_one(event)
+    eventsCollection.insert_one(event)
     return redirect('/')
 
 
